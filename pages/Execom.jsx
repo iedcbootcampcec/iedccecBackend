06b1ccf93179271c25501058cf4eaf22ae9b85c5
Jerry Sanju Joanes <jerrysanjujoanes000@gmail.com>
2024-09-21 00:49:11 +0530

    updated execome page

--- pages/Execom.jsx
+++ pages/Execom.jsx
@@ -1,23 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { firestore } from '@/utils/firebase';
-import Image from 'next/image';
+import React, { useState, useEffect } from "react";
+import { firestore } from "@/utils/firebase";
+import Image from "next/image";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsLink45Deg } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
 import { GrLinkNext } from "react-icons/gr";
 import { GrLinkPrevious } from "react-icons/gr";
-import user from "../images/user.jpg"
+import user from "../images/user.jpg";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 async function fetchData(TeamName) {
   try {
-    const TeamSnapshot = await firestore.collection(TeamName).orderBy("id").get();
-    const TeamsData = TeamSnapshot.docs.map(doc => doc.data());
+    const TeamSnapshot = await firestore
+      .collection(TeamName)
+      .orderBy("id")
+      .get();
+    const TeamsData = TeamSnapshot.docs.map((doc) => doc.data());
     return TeamsData;
   } catch (error) {
-    console.error('Error fetching data from Firestore:', error);
+    console.error("Error fetching data from Firestore:", error);
     return [];
   }
 }
@@ -29,17 +32,17 @@ const Execom = ({ TeamsData }) => {
   const [activeNo, setActiveNo] = useState(23);
   const [slidemove, setSlidemove] = useState("fade-up");
   const [TeamData, setTeamData] = useState(TeamsData);
- 
-  const [loading, setLoading] = useState(false)
+
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-    }, 500)
-  }, [activeNo])
+      setLoading(false);
+    }, 500);
+  }, [activeNo]);
 
   const handlePrevious = () => {
-    if (activeYear > 2022) {
+    if (activeYear > 2017) {
       setActiveYear(activeYear - 1);
       setActiveNo(activeNo - 1);
       setSlidemove("fade-right");
@@ -55,7 +58,7 @@ const Execom = ({ TeamsData }) => {
   };
 
   useEffect(() => {
-    setNoPrevious(activeYear <= 2022);
+    setNoPrevious(activeYear <= 2017);
     setNoNext(activeYear >= 2023);
   }, [activeYear]);
 
@@ -67,7 +70,7 @@ const Execom = ({ TeamsData }) => {
         setTeamData(newTeamData);
         setIsPending(false);
       } catch (error) {
-        console.error('Error fetching data from Firestore:', error);
+        console.error("Error fetching data from Firestore:", error);
       }
     };
 
@@ -79,64 +82,94 @@ const Execom = ({ TeamsData }) => {
   }, []);
 
   return (
-    <div className="bg-white flex flex-col justify-start items-center h-full w-full mt-20 mb-10   overflow-hidden" style={{ overflow: 'hidden' }}>
-      <div className="w-full h-fit text-center font-bold text-[50px]">Execoms</div>
+    <div
+      className="bg-white flex flex-col justify-start items-center h-full w-full mt-20 mb-10   overflow-hidden"
+      style={{ overflow: "hidden" }}
+    >
+      <div className="w-full h-fit text-center font-bold text-[50px]">
+        Execoms
+      </div>
       <div className="w-full mt-5 ml-8 text-[25px] flex gap-3">
-        <button onClick={handlePrevious} disabled={noPrevious} className={`rounded-[100%] h-10 w-10 flex justify-center items-center transition-all duration-500 ${noPrevious ? "opacity-10" : "hover:bg-slate-300 hover:scale-105 "}`}>
-          <GrLinkPrevious className='hover:scale-105' />
+        <button
+          onClick={handlePrevious}
+          disabled={noPrevious}
+          className={`rounded-[100%] h-10 w-10 flex justify-center items-center transition-all duration-500 ${
+            noPrevious ? "opacity-10" : "hover:bg-slate-300 hover:scale-105 "
+          }`}
+        >
+          <GrLinkPrevious className="hover:scale-105" />
         </button>
-        <p className='transition-all duration-1000'>{activeYear}</p>
-        <button onClick={handleNext} disabled={noNext} className={` rounded-[100%] h-10 w-10 flex justify-center items-center transition-all duration-500 ${noNext ? "opacity-10" : "hover:bg-slate-300 hover:scale-105 "}`}>
+        <p className="transition-all duration-1000">{activeYear}</p>
+        <button
+          onClick={handleNext}
+          disabled={noNext}
+          className={` rounded-[100%] h-10 w-10 flex justify-center items-center transition-all duration-500 ${
+            noNext ? "opacity-10" : "hover:bg-slate-300 hover:scale-105 "
+          }`}
+        >
           <GrLinkNext />
         </button>
       </div>
       <div className="flex flex-wrap gap-4 mt-7 justify-center">
         {loading ? (
-          <div className='h-[100vh]  '>loading....</div>
+          <div className="h-[100vh]  ">loading....</div>
         ) : (
-          TeamData && TeamData.map(item => (
-            <div key={item.name} className="w-56 mx-7 mt-14 mb-14 h-[230px] bg-white border-gray-300 rounded-2xl shadow-xl  transition-all duration-100" data-aos={slidemove}>
+          TeamData &&
+          TeamData.map((item) => (
+            <div
+              key={item.name}
+              className="w-56 mx-7 mt-14 mb-14 h-[230px] bg-white border-gray-300 rounded-2xl shadow-xl  transition-all duration-100"
+              data-aos={slidemove}
+            >
               <div className="p-2 flex flex-col items-center justify-center  transition-all duration-300 ">
                 <div className="flex items-center relative top-[-80px] shadow-lg justify-center w-44 h-44 rounded-full ">
                   <div className="w-[150px] h-[150px]">
-                   {item.image && 
-                     <Image
-                     src={`https://firebasestorage.googleapis.com/v0/b/iedccecbackend.appspot.com/o/${item.image}`}
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     alt="Circular Image"
-                     className="rounded-full w-full h-full object-cover"
-                   />
-                   }
-                   {!item.image &&
-                     <Image
-                     src={user}
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     alt="Circular Image"
-                     className="rounded-full w-full h-full object-cover"
-                   />
-                   }
+                    {item.image && (
+                      <Image
+                        src={`https://firebasestorage.googleapis.com/v0/b/iedccecbackend.appspot.com/o/${item.image}`}
+                        width={150}
+                        height={150}
+                        objectFit="cover"
+                        alt="Circular Image"
+                        className="rounded-full w-full h-full object-cover"
+                      />
+                    )}
+                    {!item.image && (
+                      <Image
+                        src={user}
+                        width={150}
+                        height={150}
+                        objectFit="cover"
+                        alt="Circular Image"
+                        className="rounded-full w-full h-full object-cover"
+                      />
+                    )}
                   </div>
                 </div>
                 <div className="text-center mt-[-65px]">
                   <h2 className="text-lg font-bold">{item.name}</h2>
                   {/* <p className="text-gray-500 text-xs">{item.pos }</p> */}
-                  {item.pos && <p className="text-gray-500 text-xs">{item.pos }</p>}
-                  {!item.pos && <p className="text-gray-500 text-xs">{item.position }</p>}
+                  {item.pos && (
+                    <p className="text-gray-500 text-xs">{item.pos}</p>
+                  )}
+                  {!item.pos && (
+                    <p className="text-gray-500 text-xs">{item.position}</p>
+                  )}
                 </div>
                 <div className="w-full h-full text-right items-center justify-start mt-3 mb-2 ml-5 flex gap-4 text-gray-600 text-xs">
                   <a href={`${item.linkedin}`} className="text-blue-500">
-                    <BsLinkedin className='h-5 w-5' />
+                    <BsLinkedin className="h-5 w-5" />
                   </a>
-                  {item.port && <a href='#'>
-                    <BsLink45Deg className='h-5 w-5 opacity-50 hover:opacity-100 transition-all duration-300'/>
-                  </a>}
-                  {item.email  && <a href={`${item.email}`}>
-                    <MdOutlineEmail className='h-6 w-6 opacity-50 hover:opacity-100 transition-all duration-300'/>
-                  </a>}
+                  {item.port && (
+                    <a href="#">
+                      <BsLink45Deg className="h-5 w-5 opacity-50 hover:opacity-100 transition-all duration-300" />
+                    </a>
+                  )}
+                  {item.email && (
+                    <a href={`${item.email}`}>
+                      <MdOutlineEmail className="h-6 w-6 opacity-50 hover:opacity-100 transition-all duration-300" />
+                    </a>
+                  )}
                 </div>
               </div>
             </div>
